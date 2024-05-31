7a9b3da3a32ec19f97e2644429cc98ead5025787
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-31 17:03:46 +0200

    [UPD] Completed layout with css

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,7 +6,28 @@ export default {
 
 <template>
   <footer>
-    <div class="container">a</div>
+    <div class="container">
+      <div class="row text-white">
+        <div class="col-50 text-align-left">
+          <p class="company">Boolando s.r.l</p>
+          <div class="footer-info">
+            <p>Informazioni legali</p>
+            <p>Normativa sulla privacy</p>
+            <p>Diritto di recesso</p>
+          </div>
+        </div>
+        <div class="col-50 text-align-right">
+          <p>Trovaci anche su</p>
+          <div class="footer-icons">
+            <i class="fa-brands fa-square-twitter"></i>
+            <i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-square-instagram"></i>
+            <i class="fa-brands fa-square-pinterest"></i>
+            <i class="fa-brands fa-square-youtube"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,7 +5,32 @@ export default {
 </script>
 
 <template>
-  Header
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <header>
+    <div class="container position-fixed">
+      <div class="row align-items-center">
+        <div class="col-25 text-align-left">
+          <ul>
+            <li><a href="">Donna</a></li>
+            <li><a href="">Uomo</a></li>
+            <li><a href="">Bambini</a></li>
+          </ul>
+        </div>
+        <div class="col-50">
+          <img class="logo" src="../assets/img/boolean-logo.png" alt="logo">
+        </div>
+        <div class="col-25 text-align-right text-white">
+          <i class="fa-regular fa-user"></i>
+          <i class="fa-regular fa-heart"></i>
+          <i class="fa-solid fa-bag-shopping"></i>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use '../src/assets/styles/main.scss';
+</style>
